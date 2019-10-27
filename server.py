2f7bb7412f38630a19ceb0f458476f0b1f8210ba
Niennte <niennte@gmail.com>
2019-10-27 22:40:12 +0300

    PEP8.

--- server.py
+++ server.py
@@ -1,30 +1,29 @@
-"""A simple HTTP server proxying requests to https://habr.com and serving its modified content.
-"""
+"""A simple HTTP server proxying requests to https://habr.com and serving its modified content."""
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.request
 import re
+
 from bs4 import BeautifulSoup, NavigableString
 
 DEFAULT_PORT = 8080
-DEFAULT_HOST = '127.0.0.1'
+DEFAULT_HOST = "127.0.0.1"
+
 
 class ProxyHandler(BaseHTTPRequestHandler):
-    """Build a minimal handler on top of BaseHTTPRequestHandler.
-    """
+    """Build a minimal handler on top of BaseHTTPRequestHandler."""
 
     REMOTE_SERVER = "https://habr.com"
     TM = "™"
 
     def do_GET(self):
-        """Override do_GET superclass method with the proxy client.
-        """
+        """Override do_GET superclass method with the proxy client."""
         self.wfile.write(self._proxy_client())
 
     def _proxy_client(self):
-        downstream_response = urllib.request.urlopen(
-            f"{self.REMOTE_SERVER}{self.path}"
-        )
+        url = f"{self.REMOTE_SERVER}{self.path}"
+        print(f"{url}\n")
+        downstream_response = urllib.request.urlopen(url)
         content = downstream_response.read()
         content_type = downstream_response.info().get_content_type()
         encoding = downstream_response.info().get_content_charset()
@@ -33,7 +32,7 @@ class ProxyHandler(BaseHTTPRequestHandler):
             content_type,
             downstream_response.getcode()
         )
-        if content_type == 'text/html':
+        if content_type == "text/html":
             content = self._process_text_content(content, encoding)
         return content
 
@@ -42,34 +41,30 @@ class ProxyHandler(BaseHTTPRequestHandler):
         self.send_header("Content-type", content_type)
         self.end_headers()
 
-    def _process_text_content(self, content, encoding='utf-8'):
-        """Modify text rendered by browser as per spec
-        """
+    def _process_text_content(self, content, encoding="utf-8"):
+        """Modify text rendered by browser as per spec."""
 
         # private helper functions
         def modify_renderable_text(match):
-            """Match words six characters long, send to be modified with the trade mark.
-            """
-            return re.sub(r'\b\w{6}\b', add_trademark_to_selected, match.group(0))
+            """Match words six characters long, send to be modified with the trade mark."""
+            return re.sub(r"\b\w{6}\b", add_trademark_to_selected, match.group(0))
 
         def add_trademark_to_selected(match):
-            """Add trade mark after input.
-            """
+            """Add trade mark after input."""
             return match.group(0) + self.TM
 
         def strip_trademarks(string):
-            """Add trade mark after input.
-            """
-            return string.replace(self.TM, '')
+            """Add trade mark after input."""
+            return string.replace(self.TM, "")
 
         try:
             content = content.decode(encoding)
             # Make links relative
-            #* - unlike the parser, regex will catch usage in scripts, svgs, etc.
-            content = content.replace(self.REMOTE_SERVER, '')
+            # * - unlike the parser, regex will catch usage in scripts, svgs, etc.
+            content = content.replace(self.REMOTE_SERVER, "")
 
-            # address the parser's bug (not rendering &plus; entity)
-            content = content.replace('&plus;', '&#43;')
+            # Address the parser's bug (not rendering &plus; entity)
+            content = content.replace("&plus;", "&#43;")
 
 
             # MODIFY TEXT:
@@ -81,15 +76,15 @@ class ProxyHandler(BaseHTTPRequestHandler):
                 """, re.VERBOSE)
             content = renderable_text_re.sub(modify_renderable_text, content)
 
-            # Unfortunately, while this is faster and simpler than parsing,
+            # While the above is faster and simpler than parsing,
             # it mangles some non-textual tags;
             # fix these with parser
-            soup = BeautifulSoup(content, 'html.parser')
+            soup = BeautifulSoup(content, "html.parser")
             tags_re = re.compile(r"""
                 ^(script|style|svg|path|code)
                 """, re.VERBOSE)
             for tag in soup.find_all(tags_re):
-                if tag.string:
+                if tag.string is not None:
                     tag_string = strip_trademarks(str(tag.string))
                     tag.string.replace_with(NavigableString(tag_string))
 
